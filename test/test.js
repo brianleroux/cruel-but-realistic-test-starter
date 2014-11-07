9b2f8d530f7fb7ccd7053a1159e7f6c8a1ac0aa3
Andy Willis <github@awillis.fastmail.com>
2014-11-07 06:25:12 +0000

    Updated due to lint

--- test/test.js
+++ test/test.js
@@ -7,7 +7,20 @@ var idx = fs.readFileSync(file, 'UTF-8');
 var $ = cheerio.load(idx);
 
 var bootstrap = {
-  version2: ['.row-fluid', '.span*', '.offset*', '.brand', '.navbar .nav', '.nav-collapse', '.nav-toggle', '.btn-navbar', '.hero-unit', '.icon-*', '.btn', '.btn-mini', '.btn-small', '.btn-large', '.alert-error', '.visible-phone', '.visible-tablet', '.visible-desktop', '.hidden-phone', '.hidden-tablet', '.hidden-desktop', '.input-block-level', '.control-group', '.control-group.warning .control-group.error .control-group.success', '.checkbox.inline .radio.inline', '.input-prepend .input-append', '.add-on', '.img-polaroid', 'ul.unstyled', 'ul.inline', '.muted', '.label', '.label-important', '.text-error', '.table .error', '.bar', '.bar-*', '.accordion', '.accordion-group', '.accordion-heading', '.accordion-body', '.accordion-inner']
+  version2: [
+    '.row-fluid', '.span*', '.offset*', '.brand', '.navbar .nav',
+    '.nav-collapse', '.nav-toggle', '.btn-navbar', '.hero-unit', '.icon-*',
+    '.btn', '.btn-mini', '.btn-small', '.btn-large', '.alert-error',
+    '.visible-phone', '.visible-tablet', '.visible-desktop', '.hidden-phone',
+    '.hidden-tablet', '.hidden-desktop', '.input-block-level',
+    '.control-group',
+    '.control-group.warning .control-group.error .control-group.success',
+    '.checkbox.inline .radio.inline', '.input-prepend .input-append',
+    '.add-on', '.img-polaroid', 'ul.unstyled', 'ul.inline', '.muted',
+    '.label', '.label-important', '.text-error', '.table .error', '.bar',
+    '.bar-*', '.accordion', '.accordion-group', '.accordion-heading',
+    '.accordion-body', '.accordion-inner'
+  ]
 };
 
 test('Sanity', function (t) {
